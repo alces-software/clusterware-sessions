98ffc4bb2ed5e95f5c95545c45c363babbc20c38
Bob Whitelock <bob.whitelock@alces-software.com>
2016-05-16 17:45:47 +0100

    Rename geometry var for consistency with CW

--- gnome/clusterware-geometry.sh
+++ gnome/clusterware-geometry.sh
@@ -1,4 +1,4 @@
 current_geometry=$(xrandr -q | awk -F 'current' -F',' 'NR==1 {gsub("( |current)",""); print $2}')
-if [ "$current_geometry" != "${cw_SESSION_GEOMETRY:-1024x768}" ] ; then
-    xrandr --output VNC-0 --mode ${cw_SESSION_GEOMETRY:-1024x768}
+if [ "$current_geometry" != "${cw_SESSION_geometry:-1024x768}" ] ; then
+    xrandr --output VNC-0 --mode ${cw_SESSION_geometry:-1024x768}
 fi
