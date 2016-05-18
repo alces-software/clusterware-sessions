a3a7befaa59b83f6ee8c79cc3001013eda7f26df
Bob Whitelock <bob.whitelock@alces-software.com>
2016-05-18 13:45:09 +0100

    Fix for sessions defaulting to largest size
    This would happen for gnome/cinnamon/xfce sessions when a geometry not
    in the list given by xrandr was given. Thanks @mjtko for the fix (ref
    https://github.com/alces-software/clusterware-sessions/pull/9#issuecomment-219998687.

--- xfce/clusterware-geometry.sh
+++ xfce/clusterware-geometry.sh
@@ -1,4 +1,8 @@
 current_geometry=$(xrandr -q | awk -F 'current' -F',' 'NR==1 {gsub("( |current)",""); print $2}')
-if [ "$current_geometry" != "${cw_SESSION_geometry:-1024x768}" ] ; then
-    xrandr --output VNC-0 --mode ${cw_SESSION_geometry:-1024x768}
+desired="${cw_SESSION_geometry:-1024x768}"
+if [ "$current_geometry" != "$desired" ] ; then
+    if ! xrandr -q | tail -n+3 | grep -q "$desired"; then
+      desired="1024x768"
+    fi
+    xrandr --output VNC-0 --mode $desired
 fi
