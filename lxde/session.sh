c4a2674dca4d05604f4fd885dcdf08526bb4f2d3
Mark Titorenko <mark.titorenko@alces-software.com>
2016-08-03 11:48:36 +0100

    Session for LXDE (Ubuntu only, not available in EPEL/CentOS). Refs #5.

---
+++ lxde/session.sh
@@ -0,0 +1,16 @@
+################################################################################
+##
+## Alces Clusterware - Trinity Desktop Environment session script
+## Copyright (c) 2016 Alces Software Ltd
+##
+################################################################################
+require process
+
+# 'Xterm*vt100.pointerMode: 0' is to ensure that the pointer does not
+# disappear when a user types into the xterm.  In this situation, some
+# VNC clients experience a 'freeze' due to a bug with handling
+# invisible mouse pointers (e.g. OSX Screen Sharing).
+echo 'XTerm*vt100.pointerMode: 0' | process_run xrdb -merge
+process_run "${cw_ROOT}"/opt/tigervnc/bin/vncconfig -nowin &
+
+lxsession
